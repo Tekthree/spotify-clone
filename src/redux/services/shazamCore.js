7215c0799f3368db2caa1019725c5b49464ea955
Tek Jones <tekthree@gmail.com>
2022-09-29 14:04:53 -0700

    adds world chart Api Call and data map on discovery page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "cb6b756048msh07e85b56dff8298p1d7e01jsnd66e51951911",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+const shazamCoreApiKey = import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY;
 
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", shazamCoreApiKey);
+
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
 });
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
